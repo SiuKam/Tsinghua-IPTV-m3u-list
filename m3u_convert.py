42bdca4f2564a2ba83b0b6365eccad9c7ccffbc3
SiuKam <ganzhaozhuo@tongji.edu.cn>
2022-03-03 10:37:51 +0800

    update general txt

--- m3u_convert.py
+++ m3u_convert.py
@@ -8,14 +8,19 @@ channels_json = request.urlopen(channels_url).read().decode('utf8')
 channels = json.loads(channels_json)
 
 f = open('channels.m3u', 'w+', encoding='utf-8')
-
 f.write('#EXTM3U\n')
-
 for i in range(len(channels['Categories'])):
     for j in range(len(channels['Categories'][i]['Channels'])):
         line_1 = '#EXTINF:-1 group-title="' + channels['Categories'][i]['Name'] + '",' + channels['Categories'][i]['Channels'][j]['Name'] + '\n'
         m3u8_url = 'https://iptv.tsinghua.edu.cn/hls/' + channels['Categories'][i]['Channels'][j]['Vid'] + '.m3u8'
         f.write(line_1)
         f.write(m3u8_url + '\n')
+f.close()
 
+f = open('channels.txt', 'w+', encoding='utf-8')
+for i in range(len(channels['Categories'])):
+    f.write(channels['Categories'][i]['Name'] + '\n')
+    for j in range(len(channels['Categories'][i]['Channels'])):
+        m3u8_url = channels['Categories'][i]['Channels'][j]['Name'] + ',https://iptv.tsinghua.edu.cn/hls/' + channels['Categories'][i]['Channels'][j]['Vid'] + '.m3u8'
+        f.write(m3u8_url + '\n')
 f.close()
